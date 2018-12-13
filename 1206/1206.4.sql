5cb5799e1be89e76081356cefbbc2b9cf0b94abf
s18001-ikeshiro <s18001@std.it-college.ac.jp>
2018-12-13 14:46:44 +0900

    syusei1206

---
+++ 1206/1206.4.sql
@@ -0,0 +1,3 @@
+select distinct location_id
+from departments
+/
--- 1206/1206.5.sql
+++ 1206/1206.5.sql
@@ -1,6 +1,3 @@
-select location_id
-from departments
-/
-select last_name || salary AS "last_name earn salary"
+select last_name || ' earn ' || salary
 from employees
 /
